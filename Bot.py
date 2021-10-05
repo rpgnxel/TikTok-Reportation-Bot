72b525de4e38deb937156c4e1a629414916de36c
H4xton <88253538+H4xton@users.noreply.github.com>
2021-10-05 16:46:49 +0200

    Update Bot.py
    Updated :)

--- Bot.py
+++ Bot.py
@@ -1,28 +1,61 @@
-import http.client
-import json
+"""
 
-conn = http.client.HTTPSConnection("www.tiktok.com")
-payload = json.dumps({
-  "reason": 1004,
-  "object_id": "6973396990155590918",
-  "owner_id": "6517992658232901642",
-  "report_type": "video"
-})
-headers = {
-  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:90.0) Gecko/20100101 Firefox/90.0',
-  'Accept': 'application/json, text/plain, */*',
-  'Accept-Language': 'da,en-US;q=0.7,en;q=0.3',
-  'Content-Type': 'application/json',
-  'Origin': 'https://www.tiktok.com',
-  'Sec-Fetch-Dest': 'empty',
-  'Sec-Fetch-Mode': 'cors',
-  'Sec-Fetch-Site': 'same-origin',
-  'Referer': 'https://www.tiktok.com/foryou?is_copy_url=1&is_from_webapp=v1&item_id=6973396990155590918',
-  'Connection': 'keep-alive',
-  'TE': 'trailers'
-}
-while True:
-    conn.request("POST", "/node/report/reasons_put?aid=1988&app_name=tiktok_web&device_platform=web_pc&device_id=6990406517787018758&region=DK&priority_region=&os=windows&referer=&root_referer=&cookie_enabled=true&screen_width=1366&screen_height=768&browser_language=da&browser_platform=Win32&browser_name=Mozilla&browser_version=5.0+(Windows)&browser_online=true&app_language=en&timezone_name=Europe%252FCopenhagen", payload, headers)
-    res = conn.getresponse()
-    data = res.read()
-    print(data.decode("utf-8"))
+
+ /$$   /$$ /$$   /$$             /$$                        
+| $$  | $$| $$  | $$            | $$                        
+| $$  | $$| $$  | $$ /$$   /$$ /$$$$$$    /$$$$$$  /$$$$$$$ 
+| $$$$$$$$| $$$$$$$$|  $$ /$$/|_  $$_/   /$$__  $$| $$__  $$
+| $$__  $$|_____  $$ \  $$$$/   | $$    | $$  \ $$| $$  \ $$
+| $$  | $$      | $$  >$$  $$   | $$ /$$| $$  | $$| $$  | $$
+| $$  | $$      | $$ /$$/\  $$  |  $$$$/|  $$$$$$/| $$  | $$
+|__/  |__/      |__/|__/  \__/   \___/   \______/ |__/  |__/
+                                                            
+                                                            
+                                                            
+
+
+
+"""
+
+import requests,os,sys,time,threading,json,random
+
+
+
+class TikTok():
+    def __init__(self):
+        self.credit = "https://github.com/h4xton"#Spoof Device ID Down Below ;)
+
+    def sendreport(self,videoid,videolink):
+        url = "https://www.tiktok.com/node/report/reasons_put?aid=1988&app_name=tiktok_web&device_platform=web_pc&region=DK&priority_region=&os=windows&referer=&cookie_enabled=true&screen_width=1920&screen_height=1080&browser_language=en-US&browser_platform=Win32&browser_name=Mozilla&browser_version=5.0+(Windows+NT+10.0%3B+Win64%3B+x64)+AppleWebKit%2F537.36+(KHTML,+like+Gecko)+Chrome%2F94.0.4606.71+Safari%2F537.36&browser_online=true&app_language=en&timezone_name=Europe%2FCopenhagen&is_page_visible=true&focus_state=true&is_fullscreen=false&history_len=3&battery_info=%7B%7D"
+        payload = json.dumps({
+                "reason": 106,
+                "object_id": videoid,
+                "owner_id": "success", ##EZ Bypass
+                "report_type": "video"
+                })
+        headers = {
+                'authority': 'www.tiktok.com',
+                'sec-ch-ua': '"Chromium";v="94", "Google Chrome";v="94", ";Not A Brand";v="99"',
+                'sec-ch-ua-mobile': '?0',
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36',
+                'content-type': 'application/json',
+                'accept': 'application/json, text/plain, */*',
+                'sec-ch-ua-platform': '"Windows"',
+                'origin': 'https://www.tiktok.com',
+                'sec-fetch-site': 'same-origin',
+                'sec-fetch-mode': 'cors',
+                'sec-fetch-dest': 'empty',
+                'referer': videolink,
+                'accept-language': 'en-US,en;q=0.9'
+                }#
+        while True:
+                   response = requests.request("POST", url, headers=headers, data=payload)
+                   receiveresponse = response.json()['errMsg']
+                   print("(TIKTOK) Succesfully Report Video: " + videoid + ' #' + receiveresponse) #Thoughs about adding rate-limit system? Cringe LUL
+
+
+
+if __name__ == "__main__":
+    videoid = input("Video ID >")
+    videolink = input("Video Link >")
+    while True: TikTok.sendreport(0,videoid,videolink)
